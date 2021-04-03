b1ed250fd5d1c7b4b143a808320bf60559c20655
Norberto-DiCaprio <webfejlesztes.kn@gmail.com>
2021-04-03 23:05:29 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+/**
+ * We use Ngx-translate for the translation.
+ * Read the documentation, how it works.
+ * https://github.com/ngx-translate/core
+ */
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+    constructor() {}
+}
